85f4d15dec0ae4e8db2df3901c55baa01fb125a8
dcatindoy <d.catindoy@arcanys.com>
2024-11-28 20:46:16 +0800

    updated readme. change preference to use mysql instead of sqlite3

--- django_project/settings.py
+++ django_project/settings.py
@@ -76,11 +76,13 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "django_project",
         "USER": "root",
         "PASSWORD": "@dmiN1234",
-        "HOST": "127.0.0.1",
+        "HOST": "localhost",
         "PORT": "3306",
     }
 }
